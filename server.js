d99f84349dcb8241f7c86ad9ee78bd7b8c65c119
Gina W <watanabegina26@gmail.com>
2017-01-07 15:46:04 -1000

    Changes to stylings in new index pug file.

--- server.js
+++ server.js
@@ -7,25 +7,25 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const CONFIG = require('./config.json');
 
-SET UP CONNECTION TO MONGO DATABASE //
-mongoose.connect(CONFIG.MONGO_URI);
+// SET UP CONNECTION TO MONGO DATABASE //
+// mongoose.connect(CONFIG.MONGO_URI);
+//
+// CHECK MONGODB CONNECTION ONCE MONGOOSE CONNECTS //
+// mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+// mongoose.connection.once('open', function(){
+//   console.log("Connected to MongoDB");
+// });
 
-CHECK MONGODB CONNECTION ONCE MONGOOSE CONNECTS //
-mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
-mongoose.connection.once('open', function(){
-  console.log("Connected to MongoDB");
-});
-
-CREATE SCHEMA & MODEL FOR 'Nake Nudeles'
-const Schema = mongoose.Schema;
-const docSchema = new Schema({
-  id: Number,
-  img: String,
-  rating: Number,
-  description: String,
-});
-
-let Doc = mongoose.model('Doc', docSchema);
+// CREATE SCHEMA & MODEL FOR 'Nake Nudeles'
+// const Schema = mongoose.Schema;
+// const docSchema = new Schema({
+//   id: Number,
+//   img: String,
+//   rating: Number,
+//   description: String,
+// });
+//
+// let Doc = mongoose.model('Doc', docSchema);
 
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -36,26 +36,26 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (req,res)=> {
   res.render("index", {});
 })
-app.get('/', (req, res) =>{
-  Doc.find()
-  .then(function (data){
-  return res.render('index', {data: data});
-  });
-});
-
-app.get('/image/:id', (req, res) => {
-  console.log(req.params);
-  Doc.find({
-    id: req.params.id
-  })
-  .then((results) => {
-    console.log(results);
-    return res.json(results);
-  });
-});
+// app.get('/', (req, res) =>{
+//   Doc.find()
+//   .then(function (data){
+//   return res.render('index', {data: data});
+//   });
+// });
+//
+// app.get('/image/:id', (req, res) => {
+//   console.log(req.params);
+//   Doc.find({
+//     id: req.params.id
+//   })
+//   .then((results) => {
+//     console.log(results);
+//     return res.json(results);
+//   });
+// });
 
 const server = app.listen(6968, () => {
   console.log(`Connected on port ${server.address().port}`);
 });
 
-module.exports = Doc;
+// module.exports = Doc;
